d1df3b0c41c09fa154f6c3e6ece27d607193eb1f
Malaiesun <59860704+malaiesun@users.noreply.github.com>
2024-10-07 12:57:29 +0530

    Update Function7-lamda.py

--- IPS-7/Function7-lamda.py
+++ IPS-7/Function7-lamda.py
@@ -1,4 +1,5 @@
 string1=input()
-######################
+########################
 result = lambda string1:string1[0].lower() in 'aeiou' if string1 else False
+#########################
 print(result(string1)
