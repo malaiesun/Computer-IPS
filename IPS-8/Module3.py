4959d5e0038a8795def915b1b89fe48bff2f65d5
Anacon <69858510+anachroni@users.noreply.github.com>
2024-11-04 12:49:20 +0530

    Update Module3.py

--- IPS-8/Module3.py
+++ IPS-8/Module3.py
@@ -6,3 +6,15 @@ char2 = input()
 
 if char1 in st or char2 in st:
     print("match")
+
+
+# this method uses the re module
+import re
+txt = input()
+a = input()[0]
+b = input()[0]
+x = re.search(r"[a.?b]",txt)
+if x:
+    print("match")
+else:
+    print("no match")
